84139179235fb20b4606ec66606350f2f944f546
Anh-vu <anhuvdinh@gmail.com>
2021-08-12 16:18:01 -0500

    quick save

--- phase-1/02-js-functions/index.js
+++ phase-1/02-js-functions/index.js
@@ -1,8 +1,63 @@
-const createPokemon = function(){
-    let pokemon = prompt('Which pokemon ar eyou today?');
-    return pokemon;
+// const createPokemon = function(){
+//     let pokemon = prompt('Which pokemon ar eyou today?');
+//     return pokemon;
+// }
+
+// createPokemon();
+
+// const renderPokemon = pokemon => console.log(`Rendering ${pokemon}`);
+
+// //finds first pokemon that starts with B - find just requires parameter becaues it's precoded to loop
+// pokemons.find((pokemon) => pokemon.startsWith('B'))
+// //To test through console, assign to a variable
+// //let results = pokemons.find((pokemon) => pokemon.startsWith('B'))
+// //console.log(results);
+
+// //arrayCollection.find((element) => function to loop)
+// pokemons.find((pokemon) => pokemon.length < 7)
+
+// //OR in same function but different form
+// pokemons.find(callBackFunction)
+// const callBackFunction = (pokemon) => pokemon.startsWith('B')
+
+// //.find() returns first match
+// //.filter() returns all matches
+
+// //for loop
+// for (let i=0; i < pokemons.length; i++){
+//     alert(pokemon[i])
+// }
+
+// //for..of loop
+// for (const pokemon of pokemons){
+//     alert(pokemon)
+// }
+
+// //forEach()
+// pokemons.forEach(pokemon => alert(pokemon))
+
+// //manipulte arrays
+// //.map returns a new array
+// function usingMap(arr){
+//     return arr.usingMap(element => lowerCasePokemon(element))
+// }
+
+// console.log(usingMap(pokemons))
+
+// let lotteryNums = [26,41,16,28,22,23]
+// const lotteryNumsCopy = [...lotteryNums]
+// let message;
+
+// lotteryNumsCopy.forEach(num => {
+//     message = `The number is ${num}`
+//     return console.log(message);
+// })
+
+let lotteryNums = [26,41,16,28,22,23]
+const lotteryNumsCopy = [...lotteryNums]
+
+for (let i=0; i < lotteryNumsCopy.length; i++){
+    const test = lotteryNumsCopy[i] = "The number is 26"
+    console.log(test)
 }
-
-createPokemon();
-
-const renderPokemon = pokemon => console.log(`Rendering ${pokemon}`);
+console.log(lotteryNumsCopy);
